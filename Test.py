a7417274b426ee5a81c3cae61a2e200347ef34b3
LindaDL <LindaMaxime@air-van-lmmt.kpn>
2021-10-31 15:24:53 +0100

    changed text

--- Test.py
+++ Test.py
@@ -1,1 +1,18 @@
 #Testing
+""" 
+*Terminal
+cd working directory (choosing repository)
+
+*VSC
+Save file in VSC
+
+*Terminal
+Git status
+Git add filename or --all
+Git commit -m "message"
+Git push
+Git status
+
+*Github
+Refresh repository
+""" 
